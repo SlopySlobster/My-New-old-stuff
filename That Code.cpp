603f2d9d40d3db8dfa396100a0c25b090bfb94a9
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2021-09-30 08:09:11 -0600

    Create That Code.cpp

---
+++ That Code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int radius;
+
+	cout << " Enter your circle radius " << endl;
+	cin >> radius;
+	cout << "your circumference is " << 2*3.14*radius << endl;
+	cout << "your area is " << 3.14 * radius * radius << endl;
+
+}
