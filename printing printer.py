b2dc0e48ec6e85a36c5cdfe370c4d0dc75f78a22
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2021-10-01 09:32:04 -0600

    Create printing printer.py

---
+++ printing printer.py
@@ -0,0 +1,29 @@
+import pygame
+
+for i in range (10):
+    print(i)
+    
+for i in range (0, 21):
+    print(i, end = ' ')
+    
+print()
+
+for i in range(12, 31):
+    print(i, end = ' ')
+    
+print()
+
+for i in range(5, 11, 2):
+    print(i, end = ' ')
+    
+myTuple = ("Mario","Luigi", "Samus", "Link", "sans")
+for i in myTuple:
+    print(i)
+    
+print(myTuple)
+print()
+
+for i in range (3):
+    for j in range(5):
+        print("*", end = ' ')
+    print()
