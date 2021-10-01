10f1bb4407dc7b83a4885cf0d2487651f03da7bd
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2021-10-01 09:36:38 -0600

    Create funny code.cpp

---
+++ funny code.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int age = 8/2546.25;
+	char initial1 = 'A';
+	char initial2 = 'B';
+	char initial3 = 'C';
+	char initial4 = 'D';
+	char initial5 = 'E';
+	char initial6 = 'F';
+	char initial7 = 'G';
+	char initial8 = 'H';
+	char initial9 = 'I';
+	char initial10 = 'J';
+	char initial11 = 'K';
+	char initial12 = 'L';
+	char initial13 = 'M';
+	char initial14 = 'N';
+	char initial15 = 'O';
+	char initial16 = 'P';
+	char initial17 = 'Q';
+	char initial18 = 'R';
+	char initial19 = 'S';
+	char initial20 = 'T';
+	char initial21 = 'U';
+	char initial22 = 'V';
+	char initial23 = 'W';
+	char initial24 = 'X';
+	char initial25 = 'Y';
+	char initial26 = 'Z';
+	float money = 0.0001;
+
+		cout << "Hello human creture!" << endl;
+
+		cout << "I see that you are an old/young person idk, you are " << age << " years old" << endl;
+		cout << "Your initials are " << initial1 << initial2 << initial3 << initial4 << initial5 << initial6 << initial7 << initial8 << initial9 << initial10 << initial11 << initial12 << initial13 << initial14 << initial15 << initial16 << initial17 << initial18 << initial19 << initial20 << initial21 << initial22 << initial23 << initial24 << initial25 << initial26 << " and you have " << money << " in your spittoon " << endl;
+		
+		cout << " (my water botel fell 3 times today) " << endl;
+}
