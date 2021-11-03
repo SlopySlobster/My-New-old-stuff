237b75da5df1042b66c32ae37cc635e509783281
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2021-11-03 10:01:42 -0600

    Update DunDungeon.cpp

--- DunDungeon.cpp
+++ DunDungeon.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main() {
 
+	string Items[10];
+	
+
 	int room = 1;
-	char input;
+	string input;
 
 	cout << " You are wake up in a cofen in a huge church. You see the doors open and you see a silowet behind a closed gate in the distance walking away, you have to get out! " << endl << endl;
 
@@ -12,7 +16,7 @@ int main() {
 		case 1:
 			cout << " You look around the church. it's dusty, like it's abandond fo years. The benches are very worn down and looks like it will break if you try to sit on them. seems like the only way out is to the north. " << endl;
 			cin >> input;
-			if (input == 'N')
+			if (input == "N")
 				room = 2;
 			else
 				cout << "You can't go there " << endl;
@@ -20,9 +24,9 @@ int main() {
 		case 2:
 			cout << " You go trough the gates. You can see " << endl;
 			cin >> input;
-			if (room == 'W')
+			if (input == "W")
 				room = 3;
-			else if (room == 'E')
+			else if (input == "E")
 				room = 5;
 			else
 				cout << "You can't go there " << endl;
@@ -30,9 +34,9 @@ int main() {
 		case 3:
 			cout << " " << endl;
 			cin >> input;
-			if (room == 'E')
+			if (input == "E")
 				room = 2;
-			else if (room == 'S')
+			else if (input == "S")
 				room = 4;
 			else
 				cout << "You can't go there " << endl;
@@ -41,25 +45,41 @@ int main() {
 		case 4:
 			cout << " " << endl;
 			cin >> input;
-			if (room == 'N')
+			if (input == "N")
 				room = 3;
+			if (input.compare("pick up") == 0)
+				Items[0] = "rusty key";
 			else
 				cout << "You can't go there " << endl;
 			break;
 		case 5:
 			cout << " " << endl;
 			cin >> input;
-			if (room == 'W')
+			if (input == "W")
 				room = 2;
-			else if (room == 'S')
+			else if (input == "S")
 				room = 6;
 			else
 				cout << "You can't go there " << endl;
 			break;
+		case 6:
+			cout << " " << endl;
+			cin >> input;
+			if (input == "N")
+				room = 5;
+			if (input.compare("go south")) {
+				if (Items[0].compare("key") != 0)
+					cout << " You tried to break open the door but it won't buge. You need a key " << endl;
+				else
+					room = 7;
+			}
+			else
+				cout << "You can't go there" << endl;
+
 		}
 	
 	
-	} while (input != 'q');
+	} while (input != "q");
 
 
 }
