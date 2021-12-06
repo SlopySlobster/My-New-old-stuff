7f690e6747d8487e0727ebad686cc7cf6eb20f51
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2021-12-06 09:15:14 -0700

    Update DunDungeon.cpp

--- DunDungeon.cpp
+++ DunDungeon.cpp
@@ -3,65 +3,82 @@
 using namespace std;
 
 //global variables (Usualy bad)
-void shop();
-string Items[10];
+
+string Items[5];
 int turns = 0;
 
 
 int main() {
 
-	
+
 	//local variables
 	int room = 1;
 	string input;
-
-	cout << " You are wake up in a cofen in a huge church. You see the doors open and you see a silowet behind a closed gate in the distance walking away, you have to get out! " << endl << endl;
+	bool inroom = false;
+	string invent = input;
+	cout << " You wake up in a cofen in a huge church. You see the doors open and you see a silhouette behind a closed gate in the distance walking away, you have to get out! " << endl << endl;
 
 	do {
-		if (input == "Items" || "items")
+		
+		
+		if (invent == "Items" || "items")
 			for (int i = 0; i < 5; i++)
 				cout << Items[i] << endl;
 		switch (room) {
 		case 1:
-			cout << " You look around the church. it's dusty, like it's abandond fo years. The benches are very worn down and looks like it will break if you try to sit on them. seems like the only way out is to the north. " << endl;
+			cout << " You look around the church. it's dusty, like it's abandoned for years. The benches are very worn down and look like they will break if you try to sit on them. seems like the only way out is to the north. " << endl;
 			cin >> input;
 			if (input == "N")
 				room = 2;
-			else if (input == "p") {
-				cout << " You found a shop under one of the curtands! " << endl;
-				shop();
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
 			}
 			else
 				cout << "You can't go there " << endl;
 			break;
 		case 2:
-			cout << " You go trough the doors. The church seems to have a grave yard, you see a huge gate in front preventing you from escaping the grave yard. There are some pathes on you east and west " << endl;
+			if (inroom == true) {
+				cout << " You are back at the entrance of the church with the gate. You can go to the east or west paths. " << endl;
+				inroom = true;
+			}
+			else if (inroom == false) {
+				cout << " You go through the doors. The church seems to have a graveyard, you see a huge gate in front preventing you from escaping the graveyard. While you were watching the area the church door's close and lock themselves.There are some paths on you east and west " << endl;
+				inroom = true;
+			}
 			cin >> input;
 			if (input == "W")
 				room = 3;
 			else if (input == "E")
 				room = 5;
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
 		case 3:
-			cout << "  " << endl;
+			cout << " The graves on the west side seem to be mostly intact, you see that the people who died are from the 19th century; meaning that the church is old. You can go south or can go back west to the entrance. " << endl;
 			cin >> input;
 			if (input == "E")
 				room = 2;
 			else if (input == "S")
 				room = 4;
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
-
 		case 4:
-			cout << " " << endl;
+			cout << " The is a huge pile of ruble in the middle of the road preventing you from continueing. You can go back north " << endl;
 			cin >> input;
 			if (input == "N")
 				room = 3;
 			else if (input.compare("pick up") == 0)
 				Items[0] = "rusty key";
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
@@ -72,6 +89,9 @@ int main() {
 				room = 2;
 			else if (input == "S")
 				room = 6;
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
@@ -83,8 +103,12 @@ int main() {
 			if (input.compare("go south")) {
 				if (Items[0].compare("key") != 0)
 					cout << " You tried to break open the door but it won't buge. You need a key " << endl;
+
 				else
 					room = 7;
+			}
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
 			}
 			else
 				cout << "You can't go there" << endl;
@@ -97,6 +121,9 @@ int main() {
 
 			else if (input == "W")
 				room = 8;
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
@@ -107,6 +134,9 @@ int main() {
 				room = 7;
 			else if (input == "W")
 				room = 9;
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
 			else
 				cout << "You can't go there " << endl;
 			break;
@@ -115,39 +145,16 @@ int main() {
 				room = 8;
 			if (input.compare("pick up") == 0)
 				Items[0] = " Wing key ";
-			else 
+			else if (input == "inventory" || input == "Inventory") {
+				cout << "Entering imventory." << endl;
+			}
+			else
 				cout << "You can't go there " << endl;
 			break;
 		}
-	
-	
+
+
 	} while (input != "q");
 
 
-}
-
-
-
-//*Write a function named "shop" that for the moment, takes no parametersand returns nothing.The function starts with a loop, and an NPC shopkeeper welcomes the player.The shopkeeper offers 3 - 4 items for sale, and asks the user if they'd like to buy something or quit. If the user selects an item, the shopkeeper places it in their inventory. 
-void shop() {
-	string input;
-	do {
-		cout << " Hoi I'm Tem, welcom to the tem shop " << endl;
-		cout << " press c to buy cheap trash ($1), t for trash($12), and e for expensive trash($500) " << endl;
-		cout << " press g for gold and m for money " << endl;
-		cout << " press q for quit" << endl;
-		cin >> input;
-		if (input == "c" || input == "t" || input == "e") {
-			Items[4] = "trash";
-			cout << " You got trash... for some reason " << endl;
-		}
-		else if (input == "g") {
-			Items[5] = "gold";
-			cout << " The gold is very heavy " << endl;
-		}
-		else if (input == "m") {
-			Items[6] = "money";
-			cout << " You got $10 " << endl;
-		}
-	} while (input != "q");
 }
