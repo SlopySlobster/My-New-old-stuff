f35cce9eff4cd9706feef91bf161cdc30f9e1082
SlopySlobster <91679115+SlopySlobster@users.noreply.github.com>
2023-02-26 21:09:19 -0700

    Update Space Invaders.py

--- Space Invaders.py
+++ Space Invaders.py
@@ -1,24 +1,74 @@
-import pygame 
+import pygame
 
-pygame.init() #set up pygame
+
+pygame.init()#set up pygame
 pygame.display.set_caption("Space Invaders!")
 screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock() #set up clock
 gameover = False #variable to run our game loop
 
+
+#player variables
+xpos = 400
+ypos = 750
+moveLeft = False
+moveRight = False
+
+
 while not gameover:
     clock.tick(60)
 
-    # Render section---------------------------
+    # Input Section-----------------------------
 
-    screen.fill((0,0,0)) # wipe screen so it doesn't 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True #quits the game if the x on the top righ corner is pressed
 
-    pygame.draw.rect(screen, (0, 200, 0), (400, 750, 60, 20)) #draw player
-    pygame.draw.rect(screen, (0, 200, 0), (405, 745, 50, 5)) #draw player
-    pygame.draw.rect(screen, (0, 200, 0), (425, 735, 10, 10)) #draw player
-    pygame.draw.rect(screen, (0, 200, 0), (428, 730, 4, 5)) #draw player
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moveLeft = True
 
-    pygame.display.flip()#flips buffer (memory) where stuff has been "drawn" to the actual screen
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moveLeft = False
 
-# game loop ends
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                moveRight = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                moveRight = False
+
+
+    # Physics section---------------------------
+
+    # checks variables from the input section
+
+    # Left movement
+    if moveLeft == True:
+        vx =- 3
+
+    if moveRight == True:
+        vx =+ 3
+
+    else:
+        vx = 0
+
+    #updates player's position
+
+    xpos += vx
+    
+    #Render section-----------------------------
+
+    screen.fill((0,0,0)) # Wipes the screen so it doesn't smear
+
+    pygame.draw.rect(screen, (0, 200, 0), (xpos, ypos, 60, 20)) #Draws player
+    pygame.draw.rect(screen, (0, 200, 0), (xpos+5, ypos-5, 50, 5)) #Draws player
+    pygame.draw.rect(screen, (0, 200, 0), (xpos+25, ypos-15, 10, 10)) #Draws player
+    pygame.draw.rect(screen, (0, 200, 0), (xpos+28, ypos-20, 4, 5)) #Draws player
+
+    pygame.display.flip()#Flips buffer (memory) where sruff has been "drawn" to the actual screen
+
+#Game loop ends
 pygame.quit()
